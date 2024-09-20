b15208d0ba8dd852b4be22a4a309822c8dbfc538
Manuel Barrabino <manubarrabino@gmail.com>
2024-09-20 13:30:20 -0300

    estructura inicial terminada

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,8 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-function pedro(hola) {
-	console.log(hola);
-}
-
 app.get('/', (req, res) => {
 	res.send('Hello World!');
-
-	pedro('hola');
 });
 
 app.listen(port, () => {
