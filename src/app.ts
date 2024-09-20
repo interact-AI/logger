e550981b8181c9e3c70aafaf9ea7b20b433645e4
Manuel Barrabino <manubarrabino@gmail.com>
2024-09-20 13:23:08 -0300

    GitHub Actions

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,16 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+function pedro(hola) {
+	console.log(hola);
+}
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+	res.send('Hello World!');
+
+	pedro('hola');
 });
 
 app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+	console.log(`Express is listening at http://localhost:${port}`);
 });
