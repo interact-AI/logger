2cc351d87fda7dfca13377b04ee066932c18234e
Manuel Barrabino <manubarrabino@gmail.com>
2024-09-24 16:35:02 -0300

    info packge update

--- src/models/info_package.ts
+++ src/models/info_package.ts
@@ -4,5 +4,8 @@ export default class InfoPackage {
 		public message: string,
 		public response: string,
 		public timeStamp: number,
+		public ownerId: string,
+		public phoneNumber: string,
+		public conversationName: string,
 	) {}
 }
